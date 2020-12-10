74e656e5474939a26d4ddc981df1df641b8d2ad3
teknoemin <teknoemin@gmail.com>
2020-12-11 00:48:10 +0300

    Add files via upload

---
+++ soru4.py
@@ -0,0 +1,7 @@
+﻿for alinan_deger in range(100, 1000):
+    ilk_kontrol = int(alinan_deger / 100)
+    ikinci_kontrol = int((alinan_deger % 100) / 10)
+    ucuncu_kontrol = int(alinan_deger % 10)
+
+    if ilk_kontrol != ikinci_kontrol and ilk_kontrol != ucuncu_kontrol and ikinci_kontrol != ucuncu_kontrol:
+        print(ilk_kontrol, ikinci_kontrol, ucuncu_kontrol, sep='')
